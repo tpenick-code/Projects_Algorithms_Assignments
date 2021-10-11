17853149555470cb33d83baa219710ba62264220
tpenick-code <77137134+tpenick-code@users.noreply.github.com>
2021-10-10 20:58:03 -0400

    Updating Week_2 Assignment

--- Week_2/Singly_Linked_Lists/Fronts/fronts.js
+++ Week_2/Singly_Linked_Lists/Fronts/fronts.js
@@ -1,9 +1,56 @@
-function double(arr){
-    for(let i=0; i<arr.length; i++){
-        arr[i]*=2
+// function double(arr){
+//     for(let i=0; i<arr.length; i++){
+//         arr[i]*=2
+//     }
+//     return arr
+// }
+// let arr=[1,2,3,4,5]
+// double(arr)
+// console.log(arr)
+
+
+//Add Front
+class SLL {
+    constructor(){
+        this.head = null;
     }
-    return arr
+    addFront (val){
+        let newNode = new SLLNode(val);
+        newNode.next = this.head
+        this.head = newNode;
+        return this.head;
+    };
 }
-let arr=[1,2,3,4,5]
-double(arr)
-console.log(arr)
+
+//Remove Front
+class SLL {
+    constructor(){
+        this.head = null;
+    }
+
+    removeFront() {
+        if (this.head ===null){
+            return this.head
+        }
+        let removeNode = this.head;
+        this.head = removedNode.next;
+        removedNode.next = null;
+        return this.head;
+    };
+}
+
+//Front
+class SLL {
+    constructor(){
+        this.head = null;
+    }
+
+    front () {
+        if (this.head ===null) {
+            return null;
+        } else {
+            return this.head.val;
+        }
+
+    };
+}
